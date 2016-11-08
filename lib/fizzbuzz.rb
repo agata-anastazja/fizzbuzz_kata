0e46f422c8ad0e3732d1d618256b565745c27364
Agata Sumowska <agata.sumowska@gmail.com>
2016-11-08 10:41:46 +0000

    round 2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz n
-  return 0 if n == 0
+  return 0 if n==0
   return "fizzbuzz" if n%15 == 0
   return "fizz" if n%3 == 0
-  return "buzz" if n%5 == 0
+  return  "buzz" if n%5 == 0
   return n
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,23 +1,19 @@
 require 'fizzbuzz'
 
 describe "fizzbuzz" do
-  it "returns fizz when divisable by 3" do
+  it "should return fizz when divisible by 3" do
   expect(fizzbuzz(3)).to eq "fizz"
   end
-  it "returns buzz when divisable by 5" do
+  it "should return buzz when divisible by 5" do
   expect(fizzbuzz(5)).to eq "buzz"
   end
-  it "returns fizzbuzz when divisable by 15" do
+  it "should return fizzbuzz when divisible by 15" do
   expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
-  it "returns number otherwise" do
-  expect(fizzbuzz(4)).to eq 4
-  end
-  it "returns 0 when passed 0" do
+  it "should return 0 when passed 0" do
   expect(fizzbuzz(0)).to eq 0
   end
+  it "should return 7 when passed 7" do
+  expect(fizzbuzz(7)).to eq 7
+  end
 end
-
-=begin
-
-=end
